1327f5c56b139be6ff9e9418e254adefd8a4c0b0
LuisCebrian <luicebch@gmail.com>
2021-02-13 10:10:48 +0100

    Initial zsh configuration

---
+++ deploy.sh
@@ -0,0 +1,82 @@
+prompt_install() {
+	echo -n "$1 is not installed. Would you like to install it? (y/n) " >&2
+	old_stty_cfg=$(stty -g)
+	stty raw -echo
+	answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+	stty $old_stty_cfg && echo
+	if echo "$answer" | grep -iq "^y" ;then
+		# This could def use community support
+		sudo apt install $1 -y
+	fi
+}
+
+check_for_software() {
+	echo "Checking to see if $1 is installed"
+	if ! [ -x "$(command -v $1)" ]; then
+		prompt_install $1
+	else
+		echo "$1 is installed."
+	fi
+}
+
+check_default_shell() {
+	if [ -z "${SHELL##*zsh*}" ] ;then
+			echo "Default shell is zsh."
+	else
+		echo -n "Default shell is not zsh. Do you want to chsh -s \$(which zsh)? (y/n)"
+		old_stty_cfg=$(stty -g)
+		stty raw -echo
+		answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+		stty $old_stty_cfg && echo
+		if echo "$answer" | grep -iq "^y" ;then
+			chsh -s $(which zsh)
+		else
+			echo "Warning: Your configuration won't work properly. If you exec zsh, it'll exec tmux which will exec your default shell which isn't zsh."
+		fi
+	fi
+}
+
+echo "We're going to setup the terminal:"
+
+echo "Let's get started? (y/n)"
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y" ;then
+	echo 
+else
+	echo "Quitting, nothing was changed."
+	exit 0
+fi
+
+git submodule update --init --recursive
+echo "1. Check to make sure you have zsh, vim, and tmux installed"
+check_for_software zsh
+echo
+check_for_software vim
+echo
+check_for_software tmux
+echo
+
+echo "2. We're going to check to see if your default shell is zsh"
+check_default_shell
+
+echo
+echo -n "Would you like to backup your current dotfiles? (y/n) "
+old_stty_cfg=$(stty -g)
+stty raw -echo
+answer=$( while ! head -c 1 | grep -i '[ny]' ;do true ;done )
+stty $old_stty_cfg
+if echo "$answer" | grep -iq "^y" ;then
+	mv ~/.zshrc ~/.zshrc.old
+	mv ~/.tmux.conf ~/.tmux.conf.old
+	mv ~/.vimrc ~/.vimrc.old
+else
+	echo -e "\nNot backing up old dotfiles."
+fi
+
+printf "source '$HOME/GIT/dotfiles/zsh/zshrc.sh'" > ~/.zshrc
+
+echo
+echo "Please log out and log back in for default shell to be initialized."
---
+++ zsh/zshrc.sh
@@ -0,0 +1,41 @@
+# Vars
+
+HISTFILE=~/.zsh_history
+SAVEHIST=1000
+ 
+setopt inc_append_history # To save every command before it is executed 
+setopt share_history      # setopt inc_append_history
+
+
+source ~/GIT/dotfiles/zsh/plugins/fixls.sh
+
+
+# Custom cd: cd + ls
+chpwd() ls
+
+
+# Load zsh plugins
+source ~/GIT/dotfiles/zsh/plugins/ohmyzsh/lib/history.zsh
+source ~/GIT/dotfiles/zsh/plugins/ohmyzsh/lib/key-bindings.zsh
+source ~/GIT/dotfiles/zsh/plugins/ohmyzsh/lib/completion.zsh
+source ~/GIT/dotfiles/zsh/plugins/ohmyzsh/plugins/vi-mode/vi-mode.plugin.zsh
+source ~/GIT/dotfiles/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+source ~/GIT/dotfiles/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+
+# Fix for arrow-key searching
+# start typing + [Up-Arrow] - fuzzy find history forward
+if [[ "${terminfo[kcuu1]}" != "" ]]; then
+	autoload -U up-line-or-beginning-search
+	zle -N up-line-or-beginning-search
+	bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search
+fi
+# start typing + [Down-Arrow] - fuzzy find history backward
+if [[ "${terminfo[kcud1]}" != "" ]]; then
+	autoload -U down-line-or-beginning-search
+	zle -N down-line-or-beginning-search
+	bindkey "${terminfo[kcud1]}" down-line-or-beginning-search
+fi
+
+
+source ~/GIT/dotfiles/zsh/prompt.sh
